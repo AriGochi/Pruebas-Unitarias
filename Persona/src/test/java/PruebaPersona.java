fc52670be565e6edcb0fc4315102ae6e9b8c8de1
AriGochi <125834764+AriGochi@users.noreply.github.com>
2023-05-19 13:48:42 -0600

    Persona 2.0
    Se agrego a las pruebas unitarias los métodos Assert.NotNull y Assert.Null

--- Persona/src/test/java/PruebaPersona.java
+++ Persona/src/test/java/PruebaPersona.java
@@ -4,13 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PruebaPersona {
-    @Test
-    public void PruebasPersona() {
-        Persona persona = new Persona("Ari", 19, 1.58, 52, "Femenino");
-        String esperado = "Ari";
-        Assertions.assertEquals(esperado, persona.getNombre());
-
-    }
 
     @Test
     public void PruebasPersona2() {
@@ -27,5 +20,65 @@ public class PruebaPersona {
         double esperado = 85;
         Assertions.assertEquals(esperado, persona3.getPeso());
     }
+    //Pruebas con NotNull y Null
+
+    @Test
+    public void testConstructorPersona() {
+
+        // Prueba para verificar que el constructor de Persona inicialice correctamente los atributos
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, "Masculino");
+
+        Assertions.assertNotNull(persona);
+        Assertions.assertEquals("Juan", persona.getNombre());
+        Assertions.assertEquals(25, persona.getEdad());
+        Assertions.assertEquals(1.75, persona.getEstatura(), 0.01);
+        Assertions.assertEquals(70.5, persona.getPeso(), 0.01);
+        Assertions.assertEquals("Masculino", persona.getGenero());
+    }
+    @Test
+    public void testNombreNoNulo() {
+        Persona persona = new Persona("Ari", 25, 1.75, 70.5, "Masculino");
+
+        Assertions.assertNotNull(persona.getNombre());
+    }
+
+    @Test
+    public void testGeneroNoNulo() {
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, "Masculino");
+
+        Assertions.assertNotNull(persona.getGenero());
+    }
+
+    @Test
+    public void testEstaturaNoNula() {
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, "Masculino");
+
+        Assertions.assertNotNull(persona.getEstatura());
+    }
+
+    @Test
+    public void testPesoNoNulo() {
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, "Masculino");
+
+        Assertions.assertNotNull(persona.getPeso());
+    }
+
+    @Test
+    public void testEdadNoNula() {
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, null);
+
+        Assertions.assertNotNull(persona.getEdad());
+        Assertions.assertNull(persona.getGenero());
+    }
+
+    @Test
+    public void testConstructorPersonaSinGenero() {
+
+        Persona persona = new Persona("Juan", 25, 1.75, 70.5, null);
+
+        Assertions.assertNotNull(persona);
+        Assertions.assertNull(persona.getGenero());
+    }
 }
+
 

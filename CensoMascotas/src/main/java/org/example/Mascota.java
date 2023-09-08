e1de73a20af843b4d15694f4af3a032588958db0
AriGochi <125834764+AriGochi@users.noreply.github.com>
2023-09-08 11:31:22 -0600

    Add files via upload

--- CensoMascotas/src/main/java/org/example/Mascota.java
+++ CensoMascotas/src/main/java/org/example/Mascota.java
@@ -1,49 +1,63 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Mascota {
-    private String Nombre;
-    private String Tipo;
-    private String Tamaño;
+    private String[] Nombre;
+    private String[] Tipo;
+    private String[] Tamaño;
 
     public Mascota() {
     }
 
-    public Mascota(String nombre, String tipo, String tamaño) {
+    public Mascota(String[] nombre, String[] tipo, String[] tamaño) {
         Nombre = nombre;
         Tipo = tipo;
         Tamaño = tamaño;
     }
 
-    public String getNombre() {
+    public String[] getNombre() {
         return Nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String[] nombre) {
         Nombre = nombre;
     }
 
-    public String getTipo() {
+    public String[] getTipo() {
         return Tipo;
     }
 
-    public void setTipo(String tipo) {
+    public void setTipo(String[] tipo) {
         Tipo = tipo;
     }
 
-    public String getTamaño() {
+    public String[] getTamaño() {
         return Tamaño;
     }
 
-    public void setTamaño(String tamaño) {
+    public void setTamaño(String[] tamaño) {
         Tamaño = tamaño;
     }
 
     @Override
     public String toString() {
         return "Mascota{" +
-                "Nombre='" + Nombre + '\'' +
-                ", Tipo='" + Tipo + '\'' +
-                ", Tamaño='" + Tamaño + '\'' +
+                "Nombre=" + Arrays.toString(Nombre) +
+                ", Tipo=" + Arrays.toString(Tipo) +
+                ", Tamaño=" + Arrays.toString(Tamaño) +
                 '}';
+    }
+
+    public void setNombre(int index, String dato) {
+        Nombre[index] = dato;
+    }
+
+    public void setTipo(int index, String dato) {
+        Tipo[index] = dato;
+    }
+
+    public void setTamaño(int index, String dato) {
+        Tamaño[index] = dato;
     }
 }
--- CensoMascotas/src/test/java/ProbarMascota.java
+++ CensoMascotas/src/test/java/ProbarMascota.java
@@ -4,36 +4,41 @@ import org.junit.jupiter.api.Test;
 
 public class ProbarMascota {
     @Test
-    public void pruebaTipoNoNulo() {
-        Mascota mascota = new Mascota("Dell", "Caballo", "Grande");
-        Assertions.assertNotNull(mascota.getTipo());
+    public void ProbarNombre() {
+        Mascota mascota = new Mascota();
+        String[] inicial = {"Kane", "Mediano", "Perro"};
+        mascota.setNombre(inicial);
+        String[] esperado = {"Kane", "Mediano", "Perro"};
+        Assertions.assertArrayEquals(esperado, mascota.getNombre());
+
     }
 
     @Test
-    public void PruebaTamañoNoNulo() {
-        Mascota mascota = new Mascota("Dell", "Caballo", "Grande");
-        Assertions.assertNotNull(mascota.getTamaño());
+    public void ProbarTamaño() {
+        Mascota mascota = new Mascota();
+        String[] inicial2 = {"Nena", "Chico", "Gato"};
+        mascota.setTamaño(inicial2);
+        String[] esperado2 = {"Nena", "Chico", "Gato"};
+        Assertions.assertArrayEquals(esperado2, mascota.getTamaño());
+
+
     }
 
     @Test
-    public void PruebaNombreNoNulo() {
-        Mascota mascota = new Mascota("Dell", "Caballo", "Grande");
-        Assertions.assertNotNull(mascota.getNombre());
+    public void ProbarTipo() {
+        Mascota mascota = new Mascota();
+        String[] inicial3 = {"Koda", "Chico", "Oso"};
+        mascota.setTipo(inicial3);
+        String[] esperado3 = {"Koda", "Chico", "Oso"};
+        Assertions.assertArrayEquals(esperado3, mascota.getTipo());
+    }
+    @Test
+    public void ProbarNombreFiferente() {
+        Mascota mascota = new Mascota();
+        String[] inicial3 = {"Koda", "Chico", "Oso"};
+        mascota.setNombre(inicial3);
+        String[] esperado3 = {"Akini", "Chico", "Oso"};
+        Assertions.assertNotEquals(esperado3, mascota.getNombre());
 
-    }
-    @Test
-    public void pruebaNombreNulo(){
-        Mascota compu = new Mascota(null,"Gato", "Chico");
-        Assertions.assertNull(compu.getNombre());
-    }
-    @Test
-    public void pruebaTipoNulo(){
-        Mascota compu = new Mascota("Kane",null, "Chico");
-        Assertions.assertNull(compu.getTipo());
-    }
-    @Test
-    public void pruebaTamañoNulo(){
-        Mascota compu = new Mascota("Molly","Gato", null);
-        Assertions.assertNull(compu.getTamaño());
     }
 }
